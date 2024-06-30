d1c72733bbbcaa39603a1ba16e660aa9961e83eb
umairyaqoob18 <umairyaqoob7860@gmail.com>
2024-06-30 13:22:51 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+import inquirer from "inquirer";
+//Asking question for inquirier
+let answer = await inquirer.prompt([
+    { message: "enter the first number", type: "number", name: "firstnumber" },
+    { message: "enter the second number", type: "number", name: "secondnumber" },
+    {
+        message: "select one operator to perfoam operations",
+        type: "list",
+        name: "operator",
+        choices: ["Additions", "subtraction", "multiply", "division"],
+    }
+]);
+// conditional statements if-Else
+if (answer.operator === "Additions") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "subtraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "multiply") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log("invalid input");
+}
